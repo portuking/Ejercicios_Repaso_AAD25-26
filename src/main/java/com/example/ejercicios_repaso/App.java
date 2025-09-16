15288c5ebbd554718dd5339cb33bd2fef8e5b4e6
portuking <dopazopablo2004@gmail.com>
2025-09-17 01:58:29 +0200

    Apartado 1: Ejercicio suma en una matriz

--- src/main/java/com/example/ejercicios_repaso/App.java
+++ src/main/java/com/example/ejercicios_repaso/App.java
@@ -4,10 +4,27 @@ package com.example.ejercicios_repaso;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void matrizEnteros() {
+        int suma = 0;
+        int[][] matriz = {
+            {1, 4, 7},
+            {2, 5, 10},
+            {6, 3, 9}
+        };
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                suma += matriz[i][j];
+            }
+            System.out.println(suma);
+            suma = 0;
+        }
+        System.out.println();
+    }
+    
+
+    public static void main( String[] args ) {
+        matrizEnteros();
     }
 }
